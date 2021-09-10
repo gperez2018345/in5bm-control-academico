5d4a9fcb462f0e8ca602565d089b3b8d177dee91
Elmer Castillo <ecastillo-2017029@kinal.edu.gt>
2021-09-09 19:06:38 -0600

    actualizando

--- src/main/java/com/in5bmgrupo4/controllers/ServletInstructorController.java
+++ src/main/java/com/in5bmgrupo4/controllers/ServletInstructorController.java
@@ -9,6 +9,7 @@ import com.in5bmgrupo4.models.dao.InstructorDaoImpl;
 import com.in5bmgrupo4.models.domain.Instructor;
 import java.io.IOException;
 import java.util.List;
+import javax.persistence.Id;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -71,5 +72,24 @@ public class ServletInstructorController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
     }
+    
+    
+    
+
+    private void insertarInstructores(HttpServletRequest request, HttpServletResponse response)throws IOException{
+        System.out.println("entrando a insertar a instructores");
+        String Id=request.getParameter("Id");
+        String apellidos=request.getParameter("apellidos");
+        String nombres=request.getParameter("nombres");
+        String direccion=request.getParameter("direccion");
+        String telefono=request.getParameter("telefono");
+        Instructor instructor= new Instructor(Id, apellidos, nombres, direccion, telefono);
+        System.out.println(instructor);
+        int registrosAgregados=new InstructorDaoImpl().insertar(instructor);
+        System.out.println("Cantidad de registros agregados: " + registrosAgregados);
+        listarInstructores(request, response);
+    }
 
 }
+  
+
